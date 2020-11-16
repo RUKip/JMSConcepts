67365b596f20de02afb330fb3306c3904e5a7ddd
Ruben <Gjr.kip@gmail.com>
2020-11-16 20:56:18 +0100

    added JMS version

--- src/main/java/Example.java
+++ src/main/java/Example.java
@@ -1,9 +1,34 @@
+import javax.jms.*;
+import javax.naming.*;
+
 public class Example {
 
     public static void main(String[] args)
     {
-        System.out.println("hello world");
-        Replier replier = Replier::newReplier();
+        try {
+            //Create a context (shared for consumer/producer)
+            InitialContext initialContext = new InitialContext();
+
+            //Lookup the location and other settings for factory and queue as defined in properties
+            ConnectionFactory connectionFactory
+                    = (ConnectionFactory) initialContext.lookup("jms/__defaultConnectionFactory");
+
+            Queue queue = (Queue) initialContext.lookup("testqueue");
+
+            JMSContext jmsContext = connectionFactory.createContext();
+
+            //Producer
+            TextMessage textMessage = jmsContext.createTextMessage("Hello world");
+            jmsContext.createProducer().send(queue, textMessage);
+
+
+            //Consumer
+            TextMessage message = (TextMessage) jmsContext.createConsumer(queue).receive();
+            System.out.println(message.getText());
+
+        }catch (Exception e){
+            System.out.println("Something went wrong :(  " + e.getMessage() );
+        }
 
     }
 }
